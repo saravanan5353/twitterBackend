dae3efb582a6f50a64ce730d0980d6191c6c0870
saravanan <saravanan.s@numero.app>
2025-01-06 18:39:23 +0530

    Init project and setup the crud operations end points for user and tweet

--- src/routes/tweetRoutes.ts
+++ src/routes/tweetRoutes.ts
@@ -2,27 +2,26 @@ import {Router} from 'express';
 
 const router =Router();
 
-//User Create End Points (Crud Operations)
- //creating User 
+//Tweet Create End Points (Crud Operations)
  router.post('/',(req,res)=>{
     res.status(501).json({error:'not Implemented'});
 });
-//List Users
+//List Tweet
 router.get('/',(req,res)=>{
    res.status(501).json({error:'not Implemented'});
 });
-// Get One User
+// Get One Tweet
 router.get('/:id',(req,res)=> {
    const {id}=req.params;
    res.status(501).json({error:`not Implemented: ${id}`});
 });
 
-//Update User
+//Update Tweet
 router.put('/:id',(req,res)=> {
    const {id}=req.params;
    res.status(501).json({error:`not Implemented: ${id}`});
 });
-// Delete User
+// Delete Tweet
 router.delete('/?:id',(req,res)=> {
    const {id}=req.params;
    res.status(501).json({error:`not Implemented: ${id}`})
